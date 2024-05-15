596e240f1acd481b290bd076763b4b3953584541
manasan-iTL <daiyaofssh2020@gmail.com>
2024-05-15 15:02:31 +0900

    ハンドラ部分を関数に切り出すリファクタ

--- js/humberger.js
+++ js/humberger.js
@@ -1,10 +1,18 @@
+// イベントターゲット要素
 const checkbox = document.getElementById("hb-menu_check");
 
-checkbox.addEventListener("change", (e) => {
-    console.log(e.target.checked)
+// イベントハンドラー
+
+/*
+    ハンバーガーメニューが表示されているときは、後ろのコンテンツ部分をスクロールできないようにする
+*/
+
+function scrollNonContent(e) {
     if (e.target.checked) {
         document.body.style.overflowY = "hidden";
     } else {
         document.body.style.overflowY = "auto";
     }
-})
+}
+
+checkbox.addEventListener("change", scrollNonContent)
